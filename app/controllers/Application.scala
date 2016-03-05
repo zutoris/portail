5310413994e1c810b716e06ddc4e1bf8daa7cfd0
Carl <zutoris1975@yahoo.fr>
2016-03-05 22:13:57 +0100

    ajout de la page vidéos

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,36 +15,47 @@ class Application extends Controller {
     Ok(views.html.musique())
   }
 
-  def congesHomePage = Action {
-	val googleDrive = new URL("https://docs.google.com/spreadsheets/d/1X-yqIgRdGK-VlBMcqLzF_xx2tlP_ylJypmemokhX4ak/edit?usp=sharing")
-    val in = new BufferedReader(new InputStreamReader(googleDrive.openStream()))
-    var inputLine = in.readLine()
-    var nbConges="-1".toLong
-    var nbRtt = "-1".toDouble
-    var textFound = false
-    while (inputLine != null && !textFound){
-		val researchedText = "CP restants : "
-		val textLength = researchedText.length()
-		val indexFound = inputLine.indexOf(researchedText)
-		if (indexFound > 0){
-			val index2Found = inputLine.indexOf("_", indexFound+textLength)
-			nbConges = inputLine.substring(indexFound+textLength, index2Found).toLong
-
-			val researchedRttText = "RTT restants : "
-			val textRttLength = researchedRttText.length()
-			val indexRttFound = inputLine.indexOf(researchedRttText)
-			if (indexRttFound > 0){
-				val index2RttFound = inputLine.indexOf("_", indexRttFound+textRttLength)
-				val nbRttInString = inputLine.substring(indexRttFound+textRttLength, index2RttFound)
-				nbRtt = nbRttInString.replace(',','.').toDouble
-			}
-			textFound = true
-		}
-		inputLine = in.readLine()
-	}
-    in.close()
-    
-    Ok(views.html.conges(nbConges, nbRtt))
+  def videoHomePage = Action {
+    Ok(views.html.videoP())
   }
+
+	/** Remplissage de la page des congés */
+	def congesHomePage = Action {
+		val googleDrive = new URL("https://docs.google.com/spreadsheets/d/1X-yqIgRdGK-VlBMcqLzF_xx2tlP_ylJypmemokhX4ak/edit?usp=sharing")
+		val in = new BufferedReader(new InputStreamReader(googleDrive.openStream()))
+		var inputLine = in.readLine()
+		var nbConges="-1".toDouble
+		var nbRtt="-1".toDouble
+		var textFound = false
+		while (inputLine != null && !textFound){
+			val researchedText = "CP restants : "
+			val textLength = researchedText.length()
+			val indexFound = inputLine.indexOf(researchedText)
+			if (indexFound > 0){
+				val index2Found = inputLine.indexOf("_", indexFound+textLength)
+				nbConges = inputLine.substring(indexFound+textLength, index2Found).replace(',','.').toDouble
+				nbRtt = getNbRttRestants(inputLine)
+				textFound = true
+			}
+			inputLine = in.readLine()
+		}
+		in.close()
+		
+		Ok(views.html.conges(nbConges, nbRtt))
+	}
+
+	/** Récupération du nombre de RTT restants */
+	def getNbRttRestants(inputLine:String) : Double = {
+		val researchedRttText = "RTT restants : "
+		val textRttLength = researchedRttText.length()
+		val indexRttFound = inputLine.indexOf(researchedRttText)
+		if (indexRttFound > 0){
+			val index2RttFound = inputLine.indexOf("_", indexRttFound+textRttLength)
+			val nbRttInString = inputLine.substring(indexRttFound+textRttLength, index2RttFound)
+			return nbRttInString.replace(',','.').toDouble
+		} else {
+			return -1
+		}
+	}
 
 }
